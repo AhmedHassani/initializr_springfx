cd5f538113124e8f3e5c40eabb330f194db047b7
Mohammed Moaayed <mohamed.moayed@gmail.com>
2020-11-18 00:05:46 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/gisse_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gisse_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\SDKflutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Fultterprojects\giesse_app"
+export "FLUTTER_APPLICATION_PATH=C:\Fultterprojects\gisse_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
