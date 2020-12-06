e182c001c1c2e9dd863f371a7764d001e9f0cf77
Mohammed Moaayed <mohamed.moayed@gmail.com>
2020-12-06 19:07:39 +0200

    floating searchBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,37 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Giesse App Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Giesse App',
-            ),
-          ],
-        ),
-      ),
+      home: MyHomePage(),
     );
   }
 }
